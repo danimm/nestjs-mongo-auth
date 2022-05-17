03e44b281801cfda6eacff27b0942deca3896a89
Daniel <daniel@exnaton.com>
2022-05-18 00:19:42 +0200

    add mongo conection with env files

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,14 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => {
   return {
+    mongo: {
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      password: process.env.MONGO_INITDB_ROOT_PASSWORD,
+      dbName: process.env.MONGO_DB,
+      host: process.env.MONGO_HOST,
+      port: parseInt(process.env.MONGO_PORT, 10),
+      conection: process.env.MONGO_CONNECTION,
+    },
     database: {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module, Global } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
+import config from '../config';
+import { ConfigType } from '@nestjs/config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
@@ -10,7 +13,25 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'MONGO',
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const {
+          user,
+          password,
+          port,
+          host,
+          dbName,
+          conection,
+        } = configService.mongo;
+        const uri = `${conection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
+        const mongoClient = new MongoClient(uri);
+        await mongoClient.connect();
+        return mongoClient.db(dbName);
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'MONGO'],
 })
 export class DatabaseModule {}
