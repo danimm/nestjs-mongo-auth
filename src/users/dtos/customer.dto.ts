20e84801b04e1c032244dafb3c54ba721b3433f4
Dani <danimm1989@gmail.com>
2022-05-28 17:30:01 +0200

    relations one to many embeded

--- src/users/dtos/customer.dto.ts
+++ src/users/dtos/customer.dto.ts
@@ -1,5 +1,19 @@
-import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsPhoneNumber,
+  ValidateNested,
+} from 'class-validator';
 import { PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+
+export class Skills {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  color: string;
+}
 
 export class CreateCustomerDto {
   @IsString()
@@ -13,6 +27,10 @@ export class CreateCustomerDto {
   @IsPhoneNumber()
   @IsNotEmpty()
   readonly phone: string;
+
+  @ValidateNested()
+  @Type(() => Skills)
+  readonly skills: Skills[];
 }
 
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
