a4ce2142519e4a43a46fb8d5932b247802cabc0f
Dani <danimm1989@gmail.com>
2022-05-28 17:42:54 +0200

    type relations from resume

--- src/products/dtos/products.dtos.ts
+++ src/products/dtos/products.dtos.ts
@@ -9,9 +9,12 @@ import {
   ValidateIf,
   ValidateNested,
   IsMongoId,
+  IsArray,
 } from 'class-validator';
 import { PartialType, ApiProperty } from '@nestjs/swagger';
 import { CreateCategoryDto } from './category.dtos';
+import { CreateSubDocDto } from './sub-doc.dto';
+import { Type } from 'class-transformer';
 
 export class CreateProductDto {
   @IsString()
@@ -48,6 +51,16 @@ export class CreateProductDto {
   @IsNotEmpty()
   @IsMongoId()
   readonly brand: string;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  readonly subDoc: CreateSubDocDto; // 👈 1:1
+
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateSubDocDto)
+  readonly subDocs: CreateSubDocDto[]; // 👈 1:N
 }
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
