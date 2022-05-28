eeb237830accb6aa36e5eb1b89b9a44dc3e2de3d
Dani <danimm1989@gmail.com>
2022-05-28 10:14:35 +0200

    mongoId pipe created

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -2,8 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { Product } from './../entities/product.entity';
-import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
+import { Product } from '../entities/product.entity';
+import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
 
 @Injectable()
 export class ProductsService {
