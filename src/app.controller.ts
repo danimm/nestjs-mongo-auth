4e4afcbf0d58b7d272537fa72a80c6e4e2e1a282
Dani <danimm1989@gmail.com>
2022-06-15 23:43:58 +0200

    first module resume: protection with guardians

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public } from './auth/decorators/public.decorator';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @Public()
   @Get('nuevo')
   newEndpoint() {
     return 'yo soy nuevo';
@@ -21,7 +26,7 @@ export class AppController {
   }
 
   @Get('/tasks')
-  getTasks() {
+  tasks() {
     return this.appService.getTasks();
   }
 }
---
+++ src/auth/guards/api-key.guard.ts
@@ -0,0 +1,37 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+import config from '../../config';
+import { ConfigType } from '@nestjs/config';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  constructor(
+    private Reflector: Reflector,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.Reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Auth');
+    const isAuth = authHeader === this.configService.apiKey;
+    if (!isAuth) {
+      throw new UnauthorizedException('Not allowed..');
+    }
+    return isAuth;
+  }
+}
