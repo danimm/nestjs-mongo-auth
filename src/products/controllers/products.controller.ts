d3541f3b2bdc6d12452b30e78f528a2da2da4123
Daniel <daniel@exnaton.com>
2022-07-02 18:36:00 +0200

    roles control in nest

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -22,8 +22,11 @@ import { ProductsService } from '../services/products.service';
 import { MongoIdPipe } from '../../common/mongo-id.pipe';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 import { Public } from '../../auth/decorators/public.decorator';
+import { Roles } from '../../auth/decorators/roles.decorator';
+import { Role } from '../../auth/models/roles.model';
+import { RolesAuthGuard } from '../../auth/guards/roles-auth.guard';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
@@ -36,6 +39,7 @@ export class ProductsController {
     return this.productsService.findAll(params);
   }
 
+  @Public()
   @Get('filter')
   getProductFilter() {
     return `yo soy un filter`;
@@ -48,11 +52,13 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id')
   update(
     @Param('id', MongoIdPipe) id: string,
@@ -61,6 +67,7 @@ export class ProductsController {
     return this.productsService.update(id, payload);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   delete(@Param('id', MongoIdPipe) id: string) {
     return this.productsService.remove(id);
